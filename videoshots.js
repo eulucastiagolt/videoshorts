41faa3fd7ff3ea4383bab69830e7a70e92974a97
Lucas Tiago <eulucastiago@outlook.com>
2026-03-08 23:34:17 -0300

    chore: bump version to 1.0.1

--- videoshots.js
+++ videoshots.js
@@ -24,7 +24,7 @@
  * SOFTWARE.
  * 
  * @author Lucas Tiago
- * @version 1.0.0
+ * @version 1.0.1
  * @license MIT
  * @repository https://github.com/eulucastiagolt/videoshorts
  */
@@ -32,7 +32,7 @@
 (function(global) {
   'use strict';
 
-  const VERSION = '1.0.0';
+  const VERSION = '1.0.1';
   const DEFAULT_OPTIONS = {
     containerClass: 'videoshort-container',
     wrapperClass: 'videoshort-wrapper',
